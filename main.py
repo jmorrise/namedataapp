464ac1cc5ac9dd31e9c38f83d4144e179f333369
Jessica Wise <jessicawise@jessicawise-macbookpro2.roam.corp.google.com>
2020-04-08 22:14:53 -0700

    Remove old demo files and use Google Charts API to graph data

--- main.py
+++ main.py
@@ -1,31 +1,26 @@
 import logging
+import numpy as np
+from src.bigquery import test_queries
 
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
-@app.route('/form')
-def form():
-    return render_template('form.html')
-
-@app.route('/submitted', methods=['POST'])
-def submitted_form():
-    name = request.form['name']
-    email = request.form['email']
-    site = request.form['site_url']
-    comments = request.form['comments']
-
+@app.route('/query')
+def query():
+    default_names = ["George", "Ronald", "Lyndon", "William", "John"]
+    names = request.args.getlist("name") or default_names
+    names = [n.capitalize() for n in names]
+    years, logps = test_queries.get_log_probs(names)
+    probs = np.exp(logps)
+    query_data = list(zip([str(y) for y in years], probs/np.sum(probs)))
+    title = ", ".join(names)
     return render_template(
-        'submitted_form.html',
-        name=name,
-        email=email,
-        site=site,
-        comments=comments)
+        'query.html', names=names, title=title,query_data=query_data)
 
 @app.errorhandler(500)
 def server_error(e):
     logging.exception('An error occurred during a request.')
     return 'An internal error occurred.', 500
-
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
---
+++ src/bigquery/test_queries.py
@@ -0,0 +1,64 @@
+from google.cloud import bigquery
+from collections import defaultdict
+from matplotlib import pyplot as plt
+import numpy as np
+
+'''
+Queries to create the BigQuery tables:
+
+logp_sample table:
+with year_counts as
+(SELECT year, sum(number) total_count
+FROM `bigquery-public-data.usa_names.usa_1910_current`
+group by year order by year)
+select name, names.year, log10(sum(number)/total_count) logp from
+`bigquery-public-data.usa_names.usa_1910_current` names
+join year_counts on names.year=year_counts.year
+group by name, year, total_count
+order by year asc, logp desc
+
+min_logp table:
+SELECT year as test_year, MIN(logp) as min_logp
+FROM `namedata.sample_dataset.logp_sample`
+group by year order by year asc;
+'''
+
+logp_table = "namedata.sample_dataset.logp_sample"
+min_logp_table = "namedata.sample_dataset.min_logp"
+unknown_penalty = 0.4
+
+"""
+Returns years and corresponding log probabilities.
+"""
+def get_log_probs(names_list, timeout_seconds=30):
+    bigquery_client = bigquery.Client()
+    query = """
+        WITH test_names AS
+        (SELECT * FROM UNNEST(@names) AS test_name CROSS JOIN {})
+        SELECT test_name name, test_year year, IFNULL(logp, min_logp - {}) logp
+        FROM `{}`
+        RIGHT OUTER JOIN test_names
+        ON LOWER(name)=LOWER(test_name) AND year=test_year
+        ORDER BY test_year ASC, test_name DESC
+    """.format(min_logp_table, unknown_penalty, logp_table)
+    job_config = bigquery.QueryJobConfig(query_parameters = [
+        bigquery.ArrayQueryParameter('names', 'STRING', names_list)
+    ])
+    query_job = bigquery_client.query(query, job_config=job_config)
+    results = query_job.result(timeout=timeout_seconds)
+    d = defaultdict(float)
+    for row in results:
+        d[row["year"]] += row["logp"]
+
+    items = sorted(d.items())
+    years = np.array([i[0] for i in items])
+    logps = np.array([i[1] for i in items])
+    return years, logps
+
+
+if __name__ == "__main__":
+    test_names = ["George", "Ronald", "Lyndon", "William", "John"]
+    years, logps = get_log_probs(test_names)
+    plt.plot(years, np.exp(logps))
+    plt.title(", ".join(test_names))
+    plt.show()
